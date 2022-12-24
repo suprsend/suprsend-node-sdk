0eeda33b4f7b9880e6e8ac786bd567863bb6816f
sivaram000 <katta.sivaram@suprsend.com>
2022-12-24 13:14:39 +0530

    removed broadcast method

--- src/subscribers_list.js
+++ src/subscribers_list.js
@@ -47,7 +47,7 @@ class SubscribersListApi {
   constructor(config) {
     this.config = config;
     this.subscriber_list_url = `${this.config.base_url}v1/subscriber_list/`;
-    this.broadcast_url = `${this.config.base_url}${this.config.workspace_key}/list_broadcast/`;
+    this.broadcast_url = `${this.config.base_url}${this.config.workspace_key}/broadcast/`;
     this.__headers = this.__common_headers();
     this.non_error_default_response = { success: true };
   }
@@ -241,56 +241,56 @@ class SubscribersListApi {
     }
   }
 
-  async broadcast(broadcast_instance) {
-    if (!(broadcast_instance instanceof SubscribersListBroadcast)) {
-      throw new SuprsendError(
-        "argument must be an instance of suprsend.SubscriberListBroadcast"
-      );
-    }
-    const [broadcast_body, body_size] = broadcast_instance.get_final_json(
-      this.config
-    );
-    const headers = { ...this.__headers, ...this.__dynamic_headers() };
-    const content_text = JSON.stringify(broadcast_body);
+  // async broadcast(broadcast_instance) {
+  //   if (!(broadcast_instance instanceof SubscribersListBroadcast)) {
+  //     throw new SuprsendError(
+  //       "argument must be an instance of suprsend.SubscriberListBroadcast"
+  //     );
+  //   }
+  //   const [broadcast_body, body_size] = broadcast_instance.get_final_json(
+  //     this.config
+  //   );
+  //   const headers = { ...this.__headers, ...this.__dynamic_headers() };
+  //   const content_text = JSON.stringify(broadcast_body);
 
-    const signature = get_request_signature(
-      this.broadcast_url,
-      "POST",
-      content_text,
-      headers,
-      this.config.workspace_secret
-    );
-    headers["Authorization"] = `${this.config.workspace_key}:${signature}`;
+  //   const signature = get_request_signature(
+  //     this.broadcast_url,
+  //     "POST",
+  //     content_text,
+  //     headers,
+  //     this.config.workspace_secret
+  //   );
+  //   headers["Authorization"] = `${this.config.workspace_key}:${signature}`;
 
-    try {
-      const response = await axios.post(this.broadcast_url, content_text, {
-        headers,
-      });
-      const ok_response = Math.floor(response.status / 100) == 2;
-      if (ok_response) {
-        return {
-          success: true,
-          status: "success",
-          status_code: response.status,
-          message: response.statusText,
-        };
-      } else {
-        return {
-          success: false,
-          status: "fail",
-          status_code: response.status,
-          message: response.statusText,
-        };
-      }
-    } catch (err) {
-      return {
-        success: false,
-        status: "fail",
-        status_code: err.status || 500,
-        message: err.message,
-      };
-    }
-  }
+  //   try {
+  //     const response = await axios.post(this.broadcast_url, content_text, {
+  //       headers,
+  //     });
+  //     const ok_response = Math.floor(response.status / 100) == 2;
+  //     if (ok_response) {
+  //       return {
+  //         success: true,
+  //         status: "success",
+  //         status_code: response.status,
+  //         message: response.statusText,
+  //       };
+  //     } else {
+  //       return {
+  //         success: false,
+  //         status: "fail",
+  //         status_code: response.status,
+  //         message: response.statusText,
+  //       };
+  //     }
+  //   } catch (err) {
+  //     return {
+  //       success: false,
+  //       status: "fail",
+  //       status_code: err.status || 500,
+  //       message: err.message,
+  //     };
+  //   }
+  // }
 }
 
 export { SubscribersListApi, SubscribersListBroadcast };
